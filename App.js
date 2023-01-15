1f54e6ee632559d8048e63a450c58d4823c920fd
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2023-01-15 21:28:59 +0900

    feat: create prctice app and etc

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}/>
+        <View style={styles.rightContainer}/>
+      </View>
     </View>
   );
 }
@@ -17,4 +20,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    backgroundColor: 'red',
+     width: 100,
+  },
+  rightContainer: {
+    backgroundColor: 'blue',
+    flex: 1,
+  }
 });
