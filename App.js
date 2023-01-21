deadcae7bd0960e0cfa63a0cd4902ae46c13ec00
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2023-01-21 18:14:42 +0900

    feat: add image and text

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
@@ -6,8 +6,23 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}/>
-        <View style={styles.rightContainer}/>
+        <View style={styles.leftContainer}>
+          <Image
+            style={ { width: 100, height: 100 } }
+            source={{ uri: 'https://picsum.photos/seed/picsum/200' }}
+            />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+          <Text>
+            ReactNews
+          </Text>
+        </View>
       </View>
     </View>
   );
@@ -28,11 +43,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   leftContainer: {
-    backgroundColor: 'red',
      width: 100,
   },
   rightContainer: {
-    backgroundColor: 'blue',
     flex: 1,
+    flexDirection: 'column',
   }
 });
